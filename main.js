a6d2d8f20c60db03200572fe05527ce880c3b62e
Anurag <Anurag979593.github.com>
2020-05-28 21:46:28 +0530

    Toursim theme website designed
    A responsive website with better SEO output has been designed. Do visit and feel free to give feedback.

---
+++ main.js
@@ -0,0 +1,6 @@
+function changer() {
+      document.body.style.backgroundImage = "url('images/ba1.jpg')";
+    }
+     function changel() {
+      document.body.style.backgroundImage = "url('images/ba2.jpg')";
+    }
